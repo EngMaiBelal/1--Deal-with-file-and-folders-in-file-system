bbb883f29748cf68abbe35aa69fd7d574579ff88
EngMaiBelal <mai.belal2127@gmail.com>
2022-10-11 00:25:46 +0200

    extension from name

--- index.php
+++ index.php
@@ -1,46 +1,47 @@
 <?php
 /////////////////////////////////////////////////////
-// EchoFunction
-
-function echoSyntax($input){
-    echo $input;
-    return "<div class='text-warning'> {$input} </div>";
-}
-
-/////////////////////////////////////////////////////
 // CreateFolder
 
 if (isset($_POST['create-folder'])) {
     if(!file_exists($_POST['folder-name']) & !empty($_POST['folder-name'])) {
         mkdir($_POST['folder-name']);
-        echoSyntax("the folder created");
+        $messageFolder ="<div class='text-warning px-4'> the folder created </div>";
+
     }elseif(file_exists($_POST['folder-name'])) {
-        echoSyntax("the folder already exist");
+        $messageFolder ="<div class='text-warning px-4'> the folder already exist </div>";
+
     }elseif(empty($_POST['folder-name'])) {
-        echoSyntax("Please insert the folder name");   
+        $messageFolder ="<div class='text-warning px-4'> Please insert the folder name </div>";
+
     }
 }
 
 /////////////////////////////////////////////////////
-// CreateFolder
+// CreateFile with radiobox
 
 function createFile()
 {
     if ($_POST['file-type'] == 'php') {
         file_put_contents($_POST['folder-thisFile-name'] . DIRECTORY_SEPARATOR . $_POST['file-name'] . '.php', '<?php ' .
             $_POST['file-content'] . ' ?>');
-        echoSyntax("The file created");
+        
+        $messageFile ="<div class='text-warning px-4'> The file created </div>";
+
 
     } elseif($_POST['file-type'] == 'txt') {
         file_put_contents($_POST['folder-thisFile-name'] . DIRECTORY_SEPARATOR . $_POST['file-name'] . '.txt', $_POST['file-content']);
-        echoSyntax("The file created");
+        $messageFile ="<div class='text-warning px-4'> The file created </div>";
+
 
     } elseif($_POST['file-type'] == 'json') {
-        file_put_contents($_POST['folder-thisFile-name'] . DIRECTORY_SEPARATOR . $_POST['file-name'] . '.json', $_POST['file-content']);
-        echoSyntax("The file created");
+        file_put_contents($_POST['folder-thisFile-name'] . DIRECTORY_SEPARATOR . $_POST['file-name'] . '.json', '{ {'.$_POST['file-content']).'} }';
+        $messageFile ="<div class='text-warning px-4'> The file created</div>";
+
 
     } elseif(empty($_POST['file-type'])) {
-        echoSyntax("Please choose the file type ");
+        $messageFile ="<div class='text-warning px-4'> Please choose the file type</div>";
+
+        
     }
 }
 
@@ -53,11 +54,56 @@ if(isset($_POST['create-file'])){
             createFile();
         }
     }elseif(empty($_POST['file-type']) || empty($_POST['folder-thisFile-name'])) {
-        echoSyntax("Please insert the folder name & folder type  & file name");
+        $messageFile ="<div class='text-warning px-4'> Please insert the folder name & folder type  & file name</div>";
+
     }
 }
 
+/////////////////////////////////////////////////////
+// CreateFile with Extension engGalal
+if(!file_exists('media')){
 
+    mkdir('media');
+}
+define('SUPPORTED_EXTENSIONS',['php','txt','json']);
+define("PATH",'media' . DIRECTORY_SEPARATOR);
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+   
+
+    if(isset($_POST['create-file-extension'])){
+        // create file
+        // pathinfo =====> array(3) { 
+                    // ["dirname"]=> string(1) "."
+                    //  ["basename"]=> string(2) "hh" 
+                    //  ["filename"]=> string(2) "hh" }
+                    // var_dump(pathinfo($_POST['file-name-extension'])['extension']);die;
+                    if(strpos($_POST['file-name-extension'],'.')){
+
+                        $fileExtension = pathinfo($_POST['file-name-extension'])['extension'];
+                        $fileName = pathinfo($_POST['file-name-extension'])['basename'];
+                        $content = $_POST['file-content-extension'];
+                        
+                        if($fileExtension == 'php'){
+                            $content = "<?php {$content} ?>";
+                        }elseif($fileExtension == 'json'){
+                            $content = "{ {$content} }";
+                        }
+                        
+                        if(! in_array($fileExtension,SUPPORTED_EXTENSIONS)){
+                            $error = "<div class='alert alert-danger text-center'> Unsupported File </div>";
+                        }else{
+                            file_put_contents(PATH  . $fileName,$content);
+                            $fileMessageWithExtension = "<div class='alert alert-success text-center'> {$fileName} Created Successfully </div>";
+                        }
+                    }else{
+                        $error = "<div class='alert alert-danger text-center'> File Name uncorrectly, Not Supported File </div>";
+
+                    }
+        // file_put_contents(PATH  . $fileName,$content);
+        // $fileMessageWithExtension = "<div class='alert alert-success text-center'> {$fileName} Created Successfully </div>";
+    }
+}
 
 ?>
 
@@ -79,7 +125,9 @@ if(isset($_POST['create-file'])){
     ">
     <div class="container py-5 " style="width:70%">
         <form class="border border-info rounded" style="background-color:rgb(0,0,0);" method="POST">
+            
             <h5 style="text-align:center;">CREATE FOLDER</h5>
+            <?= $messageFolder ??  "" ?>
             <div class="row flex ">
 
                 <div class="col-12 p-5">
@@ -93,6 +141,8 @@ if(isset($_POST['create-file'])){
         </form>
         <form class="border border-info rounded" style="background-color:rgb(0,0,0);" method="POST">
             <h5 style="text-align:center;">CREATE FILE</h5>
+            <?= $messageFile ??  "" ?>
+
             <div class="row flex ">
                 <div class="col-4 p-5">
                     <label for="exampleFormControlInput0" class="text-info" style="font-weight: bold;">Folder Name
@@ -122,6 +172,32 @@ if(isset($_POST['create-file'])){
                     <label for="exampleFormControlTextarea1" class="text-info" style="font-weight: bold;">The Content of
                         file</label>
                     <textarea name="file-content" class="mx-3" id="exampleFormControlTextarea1" rows="4" cols="20"></textarea>
+                </div>
+            </div>
+        </form>
+
+        <form class="border border-info rounded" style="background-color:rgb(0,0,0);" method="POST">
+            <h5 style="text-align:center;">CREATE FILE</h5>
+            <?= $fileMessageWithExtension ??  "" ?>
+            <?= $error ??  "" ?>
+
+            <div class="row flex ">
+                <div class="col-4 p-5">
+                    
+
+                    <label for="exampleFormControlInput0" class="text-info" style="font-weight: bold;">File Name
+                        :</label>
+                    <input name="file-name-extension" type="text" class="mx-3" id="exampleFormControlInput0">
+                    <small> Supported Files <b><?= implode(' , ',SUPPORTED_EXTENSIONS) ?> </b>
+                    
+                    <button name="create-file-extension" class="btn btn-primary m-3">Create File</button>
+
+                </div>
+                
+                <div class="col-4 p-5">
+                    <label for="exampleFormControlTextarea1" class="text-info" style="font-weight: bold;">The Content of
+                        file</label>
+                    <textarea name="file-content-extension" class="mx-3" id="exampleFormControlTextarea1" rows="4" cols="20"></textarea>
                 </div>
             </div>
         </form>
